c89feacfef67ce33cbf92122dd0448029aa9b1d9
Administrator <Administrator@DESKTOP-1B1BE1O>
2024-03-24 13:43:09 +0530

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using myfirstWebApp.Models;
+using myfirstWebApp.Services;
+
+namespace myfirstWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        public List<Product> Products;
+
+        public void OnGet()
+        {
+            ProductService productService = new ProductService();
+            Products = productService.GetProducts();
+        }
+    }
+}
